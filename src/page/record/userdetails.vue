0a999c4eb25deb5e54acb5af5dc7d67e7b89e2da
valiner <819465103@qq.com>
2017-10-18 20:31:43 +0800

    m

--- src/page/record/userdetails.vue
+++ src/page/record/userdetails.vue
@@ -3,8 +3,20 @@
   <x-header :left-options="{showBack: true}" :right-options="{showMore: true}" @on-click-more="showMenus = true">{{username}}</x-header>
   <div class="main_usd">
     <div class="usd_top">
-      <div><img :src="iconurl"></div>
-      <div><span></span><span></span></div>
+      <blur :blur-amount=40 :url="iconurl">
+      <p class="center"><img :src="iconurl"></p>
+      </blur>
+      <div class="usd_info">
+        <span class="sp1">{{datalist.game_zone.alias}}:{{datalist.pn}}</span>
+        <span class="sp2">段位：{{ranklist[datalist.tier-1]}}{{datalist.rank}}</span>
+      </div>
+    </div>
+    <div class="usd_history" v-if='datalist.formatted_ranked_history'>
+      <group label-width="5em" title="战绩概要">
+      <cell title="S7单双排" value="">胜场：{{datalist.formatted_ranked_history.s7.w}}  总场: {{datalist.formatted_ranked_history.s7.w+datalist.formatted_ranked_history.s7.lo}} 段位:{{ranklist[datalist.tier-1]}}{{datalist.rank}}</cell>
+      <cell title="s6排位" value="">胜场：{{datalist.formatted_ranked_history.s6.w}}  总场: {{datalist.formatted_ranked_history.s7.w+datalist.formatted_ranked_history.s6.lo}} 段位:{{ranklist[datalist.formatted_ranked_history.s6.t-1]}}</cell>
+     </group>
+      
     </div>
   </div>
     
@@ -13,8 +25,9 @@
 </template>
 
 <script>
-import { Tab, TabItem, Tabbar, TabbarItem, XHeader, Selector, Group, XInput, XButton } from 'vux'
+import { Tab, TabItem, Tabbar, TabbarItem, XHeader, Selector, Group, XInput, XButton, Blur, Cell } from 'vux'
 // import { searchRecord } from '../../service/getData'
+const ranklist = () => ['青铜', '白银', '黄金', '白金', '钻石', '大师', '王者']
 export default {
   components: {
     Tab,
@@ -25,14 +38,17 @@ export default {
     Group,
     XInput,
     XButton,
-    XHeader
+    XHeader,
+    Blur,
+    Cell
   },
   data () {
     return {
       area: '',
       username: '',
-      datalist: '',
-      iconurl: ''
+      datalist: {},
+      iconurl: '',
+      ranklist: ranklist()
     }
   },
   mounted () {
@@ -42,8 +58,8 @@ export default {
     this.$axios.get('/api/searchRecord/' + this.area + '/' + this.username).then(function (response) {
       console.log(response.data)
       if (response.data.status === 0) {
-        that.userdata = response.data.data
-        that.iconurl = 'http://static.lolbox.duowan.com/images/profile_icons/' + that.userdata.icon + '.jpg'
+        that.datalist = response.data.data
+        that.iconurl = 'http://static.lolbox.duowan.com/images/profile_icons/' + that.datalist.icon + '.jpg'
         // console.log(this.datalist)
       } else {
         alert(response.data.message)
@@ -62,8 +78,33 @@ export default {
 @import '~vux/src/styles/1px.less';
 @import '~vux/src/styles/center.less';
 
+.center {
+  text-align: center;
+  padding-top: 20px;
+  color: #fff;
+  font-size: 18px;
+}
+.center img {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  border: 4px solid #ececec;
+}
+.usd_info{
+}
+.usd_info .sp1{
+  position:absolute;
+  bottom:15px;
+  left:20px
+}
+.usd_info .sp2{
+  position:absolute;
+  right:20px;
+  bottom:15px;
+}
 .usd_top{
-  height:100px;
+  position:relative;
+  height:200px;
 }
 .main_usd{
   padding-top:46px;
